adf25e7da38f6b77508241a83ebd662a64bf8cbd
Bobby <bobbycair@gmail.com>
2021-09-29 21:32:37 +0800

    Added simple layout and dummy components

---
+++ main-app/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import '../style/Header.css'
+
+class Header extends React.Component {
+    render() {
+       return (
+          <div className="Header">
+             <h1>My Collections</h1>
+          </div>
+       );
+    }
+ }
+
+export default Header;
